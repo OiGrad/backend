42401f3b364d138e7e83f58b2798a8eecb31f6a4
EslamQadri <eslamkadryuwk@gmail.com>
2023-05-06 20:07:28 +0300

    get user info endpoint

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,21 @@
 from django.urls import path
 
-from users.views import SignupView, LoginView,FavoritePlaceView,AddFavoritePlaceView
+from users.views import (
+    SignupView,
+    LoginView,
+    FavoritePlaceView,
+    AddFavoritePlaceView,
+    GetUserInfo,
+)
 
-app_name = 'users'
+app_name = "users"
 
 urlpatterns = [
-    path('signup/', SignupView.as_view(), name='signup'),
-    path('singin',LoginView.as_view(),name='login'),
-    path('favorite-places/', FavoritePlaceView.as_view(), name='favorite-places'),
-    path('add-favorite-places/', AddFavoritePlaceView.as_view(), name='favorite-places'),
-
+    path("signup/", SignupView.as_view(), name="signup"),
+    path("singin", LoginView.as_view(), name="login"),
+    path("favorite-places/", FavoritePlaceView.as_view(), name="favorite-places"),
+    path(
+        "add-favorite-places/", AddFavoritePlaceView.as_view(), name="favorite-places"
+    ),
+    path("get-user-date", GetUserInfo, name="get-user"),
 ]
