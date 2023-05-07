f6743220bcf1e4f9eca94d0c1b75d22e4390c9d2
EslamQadri <eslamkadryuwk@gmail.com>
2023-05-07 03:54:52 +0300

    edit in road

--- roads/serializers.py
+++ roads/serializers.py
@@ -1,23 +1,38 @@
 from rest_framework.serializers import ModelSerializer
-from roads.models import PlaceWay ,Road
-from places.models import Area
-from places.serializers import PlaceSerializer
+from roads.models import PlaceWay, Road
+from places.models import Area, Place
+
+
+class shortPlaceSerializer(ModelSerializer):
+    class Meta:
+        model = Place
+        fields = (
+            "id",
+            "name",
+        )
+
 
 class AreaSerializer(ModelSerializer):
-    class Meta :
-        model= Area
-        fields = '__all__'
+    class Meta:
+        model = Area
+        fields = (
+            "id",
+            "name",
+        )
+
 
 class PlaceWaySerializer(ModelSerializer):
-    frm=AreaSerializer()
-    to=PlaceSerializer()
-    class Meta :
-        model= PlaceWay
-        fields = '__all__'
+    frm = AreaSerializer()
+    to = shortPlaceSerializer()
+
+    class Meta:
+        model = PlaceWay
+        fields = "__all__"
+
 
 class RoadSerializer(ModelSerializer):
-    placeWay=PlaceWaySerializer(read_only=True)
-    class Meta :
-        model= Road
-        fields = '__all__'
+    placeWay = PlaceWaySerializer(read_only=True)
 
+    class Meta:
+        model = Road
+        fields = "__all__"
